95e5ba4c18c0ed41d5ad3c6c88d4102157731952
jgaffuri <julien.gaffuri@ec.europa.eu>
2016-06-22 18:38:32 +0200

    first version of coicop-time-stack

--- js/pr-coicop-time-stack.js
+++ js/pr-coicop-time-stack.js
@@ -7,13 +7,15 @@
  */
 (function($, PrVis) {
 	$(function() {
+		//http://bl.ocks.org/mbostock/4060954
+		//http://bl.ocks.org/mbostock/3943967
 
 		//
-		PrVis.modifyCoicopHierarchy();
+		//PrVis.modifyCoicopHierarchy();
 		//TODO remove too low level coicops.
 
-		//geo/coicop/year
-		var dataIndex = {};
+		//TODO add ECB
+		var coicops = ["CP01","CP02","CP03","CP04","CP05","CP06","CP07","CP08","CP09","CP10","CP11","CP12"];
 
 		//colors
 		var color = colorbrewer.Set3[12];
@@ -36,19 +38,20 @@
 				if(out) return out;
 			}
 		};
+		var color = d3.scale.linear().range(["#aad", "#556"]); //TODO deprecated
 
 		//title
 		var title = $("#title");
 
-		//svg element
-		var w=600, h=300;
+		//svg, with a bottom-right origin.
+		var width = 960, height = 500;
 		var svg = d3.select("#stack").append("svg")
-		.attr("width", w).attr("height", h)
-		.append("g");
+		.attr("width", width)
+		.attr("height", height);
 
 		//legend
 		var lgd = $("#legend");
-		lgd.css("height", h);
+		lgd.css("height", height);
 		var infoDiv = null;
 		var refreshLegend = function(){
 			lgd.empty();
@@ -66,10 +69,9 @@
 
 		//get base information on years and geos
 		$.when($.ajax({url:EstLib.getEstatDataURL("prc_hicp_inw",{coicop:"CP00"})}))
-		.then(function(ds) {
-			EstLib.overrideCountryNames(ds.dimension.geo.category.label);
-			ds = JSONstat(ds).Dataset(0);
-			var geos = ds.Dimension("geo").id;
+		.then(function(geoDim) {
+			EstLib.overrideCountryNames(geoDim.dimension.geo.category.label);
+			geoDim = JSONstat(geoDim).Dataset(0).Dimension("geo");
 
 			//option
 			var cType = $("#ctype");
@@ -80,25 +82,60 @@
 			cType.buttonset();
 
 			//build geolist
-			PrVis.fillGeoList(geoList, geos, function(geo){return ds.Dimension("geo").Category(geo).label;});
+			PrVis.fillGeoList(geoList, geoDim.id, function(geo){return geoDim.Category(geo).label;});
 			$('#geoList option[value="EA"]').attr('selected', 'selected');
 			geoList.selectmenu({
 				change:function(){
-					$("#geoTXT").text( ds.Dimension("geo").Category(geoList.find(":selected").attr("value")).label );
+					$("#geoTXT").text( geoDim.Category(geoList.find(":selected").attr("value")).label );
 					updateChart();
 				}
 			})
 			.selectmenu("menuWidget").css("height","200px")/*.css("font-size","70%")*/;
-			$("#geoTXT").text( ds.Dimension("geo").Category(geoList.find(":selected").attr("value")).label );
+			$("#geoTXT").text( geoDim.Category(geoList.find(":selected").attr("value")).label );
 
 			var updateChart = function(){
 				var geoSel = geoList.find(":selected").attr("value");
-				console.log(geoSel);
-
-				refreshLegend();
 
 				//get data and update chart
-				//PrVis.getProductWeightData(geoSel, yearSel, dataIndex, function(){ updateChart_(dataIndex[yearSel][geoSel],yearSel,geoSel); });
+				$.when(
+						$.ajax({url:EstLib.getEstatDataURL("prc_hicp_inw",{geo:geoSel,coicop:coicops})})
+				).then(function(ds) {
+					EstLib.overrideCountryNames(ds.dimension.geo.category.label);
+					ds = JSONstat(ds).Dataset(0);
+
+					//clear previous
+					svg.selectAll("*").remove();
+
+					var n = coicops.length;
+					var yearMin = 1996, yearMax = 2016; //TODO replace
+					var stack = d3.layout.stack().offset("zero");
+
+					var data = [];
+					for(var c=0; c<coicops.length; c++){
+						var data_ = [];
+						for(var year=yearMin; year<yearMax; year++){
+							var value = ds.Data({coicop:coicops[c],time:""+year,geo:geoSel}).value;
+							data_.push({x:year,y0:0,y:value});
+						}
+						data.push(data_);
+					}
+					data = stack(data);
+
+					var x = d3.scale.linear().domain([yearMin, yearMax]).range([0, width]);
+					var y = d3.scale.linear().domain([0, 1000]).range([height, 0]);
+
+					//area construction function
+					var area = d3.svg.area()
+					.x(function(d) { return x(d.x); })
+					.y0(function(d) { return y(d.y0); })
+					.y1(function(d) { return y(d.y0 + d.y); });
+
+					svg.selectAll("path").data(data).enter().append("path").attr("d", area)
+					.style("fill", function() { return color(Math.random()); }); //TODO use coicopToColor
+				}, function() {
+					console.log("Could not load data"); //TODO better
+				});
+
 			};
 
 			//
