32799c4d4ddfbcd94fa21ffd438852cfe5ee34d2
jgaffuri <julien.gaffuri@ec.europa.eu>
2016-06-22 09:37:13 +0200

    even simpler

--- js/pr-vis-table1.js
+++ js/pr-vis-table1.js
@@ -6,35 +6,26 @@
 (function($, PrVis) {
 
 	$(function() {
-		//http://ec.europa.eu/eurostat/statistics-explained/images/2/2b/Euro_area_annual_inflation_and_its_main_components_%28%25%29%2C_2015%2C_September_2014_and_April_-_September_2015-est.png
 
 		//coicop codes corresponding to table raws
 		var coicops = ["CP00", "TOT_X_NRG", "TOT_X_NRG_FOOD", "TOT_X_NRG_FOOD_NP", "FOOD", "FOOD_P", "FOOD_NP", "NRG", "IGD_NNRG", "SERV"];
 
-
-		//things to do in the end, when all ajax have returned
-		var weightsLoaded = false, indicesLoaded = false;
-		var doEnd = function(){
-			if(!weightsLoaded || !indicesLoaded) return;
-
-			//show as an image
-			html2canvas($("#table1"), { onrendered: function(canvas) { $("#img-out").empty(); $("#img-out").append(canvas); }});
-
-			//show html code
-			$("#table1html").text( $("#table1").html() );
-		};
+		$.when(
+				//get weight data
+				$.ajax({url:EstLib.getEstatDataURL("prc_hicp_inw",{geo:"EA",coicop:coicops,lastTimePeriod:1}, null, null, "2.1")}),
+				//get rate of change (ROC) data
+				$.ajax({url:EstLib.getEstatDataURL("prc_hicp_manr",{geo:"EA",unit:"RCH_A",coicop:coicops,lastTimePeriod:7}, null, null, "2.1")})
+		).then(function(weightData, rocData) {
+			var ds;
 
 
-		//get weight data fill first table column
-		$.ajax({url:EstLib.getEstatDataURL("prc_hicp_inw",{geo:"EA",coicop:coicops,lastTimePeriod:1}, null, null, "2.1")})
-		.done(function(data) {
-			//decode data
-			var ds = JSONstat(data).Dataset(0);
+			//decode weight data
+			ds = JSONstat(weightData).Dataset(0);
 
 			//write weight year in table header
 			$("#wlabely").text( ds.Dimension("time").Category(0).label ).removeAttr("id");
 
-			//fill weights column
+			//fill weight column
 			for(var i=0; i<coicops.length; i++){
 				//get weight data
 				var coicop = coicops[i];
@@ -44,19 +35,11 @@
 				//fill table cell
 				$("#w"+coicop).text(value).removeAttr("id");
 			}
-		}).fail(function() {
-			console.log("Could not retrieve EA product weights");
-		}).always(function() {
-			weightsLoaded = true;
-			doEnd();
-		});
 
 
-		//get ROC data for the last 7 months and fill first columns
-		$.ajax({url:EstLib.getEstatDataURL("prc_hicp_manr",{geo:"EA",unit:"RCH_A",coicop:coicops,lastTimePeriod:7}, null, null, "2.1")})
-		.done(function(data) {
-			//decode data
-			var ds = JSONstat(data).Dataset(0);
+
+			//decode rate of change data
+			var ds = JSONstat(rocData).Dataset(0);
 
 			//retrieve months
 			var months = ds.Dimension("time").Category();
@@ -68,7 +51,7 @@
 				var y=month.substring(0,4), m=month.substring(5,7);
 				$("<td>").html(moment(y+"-"+m,"YYYY-MM").format("MMM YYYY")).appendTo(hrow);
 			}
-			$("#hrow").removeAttr("id"); //clean
+			hrow.removeAttr("id");
 
 			//build table cells and fill with ROC data
 			for(var i=0; i<coicops.length; i++){
@@ -81,17 +64,23 @@
 					var d = ds.Data({coicop:coicop,time:month});
 					var value = d? d3.round(d.value, 1).toFixed(1) + (d.status||"") : ":";
 
+					//last month in bold
+					if(j==(months.length-1)) value = "<b>"+value+"</b>";
+
 					//fill table cell
-					if(j==(months.length-1)) $("<td>").html("<b>"+value+"</b>").appendTo(row);
-					else $("<td>").appendTo(row).text(value);
+					$("<td>").html(value).appendTo(row);
 				}
 			}
-		}).fail(function() {
-			console.log("Could not retrieve EA rates of change");
-		}).always(function() {
-			indicesLoaded = true;
-			doEnd();
-		});
+
+			//show as an image
+			html2canvas($("#table1"), { onrendered: function(canvas) { $("#img-out").empty(); $("#img-out").append(canvas); }});
+
+			//show html code
+			$("#table1html").text( $("#table1").html() );
+
+		}, function() {
+			console.log("Could not load data"); //TODO better
+		});		
 
 	});
 }(jQuery, window.PrVis = window.PrVis || {} ));
