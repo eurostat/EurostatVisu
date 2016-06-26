d585e38cd24a59d928b59fcb383776977ce69045
julien Gaffuri <julien.gaffuri@gmail.com>
2016-06-26 21:24:53 +0200

    corrections

--- js/pr-vis-table1.js
+++ js/pr-vis-table1.js
@@ -1,87 +1,88 @@
 /**
- * 
+ *
  * @author julien Gaffuri
  *
  */
-(function($, PrVis) {
+(function($) {
 
-	$(function() {
-		//http://ec.europa.eu/eurostat/statistics-explained/index.php/Inflation_in_the_euro_area
+    $(function() {
+        //http://ec.europa.eu/eurostat/statistics-explained/index.php/Inflation_in_the_euro_area
 
-		//coicop codes corresponding to table raws
-		var coicops = ["CP00", "TOT_X_NRG", "TOT_X_NRG_FOOD", "TOT_X_NRG_FOOD_NP", "FOOD", "FOOD_P", "FOOD_NP", "NRG", "IGD_NNRG", "SERV"];
+        //coicop codes corresponding to table raws
+        var coicops = ["CP00", "TOT_X_NRG", "TOT_X_NRG_FOOD", "TOT_X_NRG_FOOD_NP", "FOOD", "FOOD_P", "FOOD_NP", "NRG", "IGD_NNRG", "SERV"];
 
-		$.when(
-				//get weight data
-				$.ajax({url:EstLib.getEstatDataURL("prc_hicp_inw",{geo:"EA",coicop:coicops,lastTimePeriod:1})}),
-				//get rate of change (ROC) data
-				$.ajax({url:EstLib.getEstatDataURL("prc_hicp_manr",{geo:"EA",unit:"RCH_A",coicop:coicops,lastTimePeriod:7})})
-		).then(function(weightData, rocData) {
-			var ds;
-
-
-			//decode weight data
-			ds = JSONstat(weightData).Dataset(0);
-
-			//write weight year in table header
-			$("#wlabely").text( ds.Dimension("time").Category(0).label ).removeAttr("id");
-
-			//fill weight column
-			for(var i=0; i<coicops.length; i++){
-				//get weight data
-				var coicop = coicops[i];
-				var d = ds.Data({coicop:coicop});
-				var value = d? d3.round(d.value, 1) + (d.status||"") : ":";
-
-				//fill table cell
-				$("#w"+coicop).text(value).removeAttr("id");
-			}
+        $.when(
+            //get weight data
+            $.ajax({url:EstLib.getEstatDataURL("prc_hicp_inw",{geo:"EA",coicop:coicops,lastTimePeriod:1})}),
+            //get rate of change (ROC) data
+            $.ajax({url:EstLib.getEstatDataURL("prc_hicp_manr",{geo:"EA",unit:"RCH_A",coicop:coicops,lastTimePeriod:7})})
+        ).then(function(weightData, rocData) {
+                var ds;
 
 
+                //decode weight data
+                ds = JSONstat(weightData).Dataset(0);
 
-			//decode rate of change data
-			var ds = JSONstat(rocData).Dataset(0);
+                //write weight year in table header
+                $("#wlabely").text( ds.Dimension("time").Category(0).label ).removeAttr("id");
 
-			//retrieve months
-			var months = ds.Dimension("time").Category();
+                //fill weight column
+                for(var i=0; i<coicops.length; i++){
+                    //get weight data
+                    var coicop = coicops[i];
+                    var d = ds.Data({coicop:coicop});
+                    var value = d? d3.round(d.value, 1) + (d.status||"") : ":";
 
-			//build header cells and fill with month labels
-			var hrow = $("#hrow");
-			for(var j=0; j<months.length; j++){
-				var month = months[j].label;
-				var y=month.substring(0,4), m=month.substring(5,7);
-				$("<td>").html(moment(y+"-"+m,"YYYY-MM").format("MMM YYYY")).appendTo(hrow);
-			}
-			hrow.removeAttr("id");
+                    //fill table cell
+                    $("#w"+coicop).text(value).removeAttr("id");
+                }
 
-			//build table cells and fill with ROC data
-			for(var i=0; i<coicops.length; i++){
-				var coicop = coicops[i];
-				var row = $("#row"+coicop).removeAttr("id");
-				for(var j=0; j<months.length; j++){
-					var month = months[j].label;
 
-					//get ROC data
-					var d = ds.Data({coicop:coicop,time:month});
-					var value = d? d3.round(d.value, 1).toFixed(1) + (d.status||"") : ":";
 
-					//last month in bold
-					if(j==(months.length-1)) value = "<b>"+value+"</b>";
+                //decode rate of change data
+                ds = JSONstat(rocData).Dataset(0);
 
-					//fill table cell
-					$("<td>").html(value).appendTo(row);
-				}
-			}
+                //retrieve months
+                var months = ds.Dimension("time").Category();
 
-			//show as an image
-			html2canvas($("#table1"), { onrendered: function(canvas) { $("#img-out").empty(); $("#img-out").append(canvas); }});
+                //build header cells and fill with month labels
+                var hrow = $("#hrow");
+                for(j=0; j<months.length; j++){
+                    var month = months[j].label;
+                    var y=month.substring(0,4), m=month.substring(5,7);
+                    $("<td>").html(moment(y+"-"+m,"YYYY-MM").format("MMM YYYY")).appendTo(hrow);
+                }
+                hrow.removeAttr("id");
 
-			//show html code
-			$("#table1html").text( $("#table1").html() );
+                //build table cells and fill with ROC data
+                for(i=0; i<coicops.length; i++){
+                    coicop = coicops[i];
+                    var row = $("#row"+coicop).removeAttr("id");
+                    for(var j=0; j<months.length; j++){
+                        month = months[j].label;
 
-		}, function() {
-			console.log("Could not load data"); //TODO better
-		});		
+                        //get ROC data
+                        d = ds.Data({coicop:coicop,time:month});
+                        value = d? d3.round(d.value, 1).toFixed(1) + (d.status||"") : ":";
 
-	});
-}(jQuery, window.PrVis = window.PrVis || {} ));
+                        //last month in bold
+                        if(j==(months.length-1)) value = "<b>"+value+"</b>";
+
+                        //fill table cell
+                        $("<td>").html(value).appendTo(row);
+                    }
+                }
+
+                //show as an image
+                var table1 = $("#table1"), img_out = $("#img-out");
+                html2canvas(table1, { onrendered: function(canvas) { img_out.empty(); img_out.append(canvas); }});
+
+                //show html code
+                $("#table1html").text( table1.html() );
+
+            }, function() {
+                console.log("Could not load data");
+            }
+        );
+    });
+}(jQuery));
