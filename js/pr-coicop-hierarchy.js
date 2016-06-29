9f352577031072a46b0a3e84947d9f97b0bb988a
jgaffuri <julien.gaffuri@ec.europa.eu>
2016-06-29 15:53:09 +0200

    a

--- js/pr-coicop-hierarchy.js
+++ js/pr-coicop-hierarchy.js
@@ -48,9 +48,6 @@
 			}
 			data["CP00"].childrenCodes = ["CP01","CP02","CP03","CP04","CP05","CP06","CP07","CP08","CP09","CP10","CP11","CP12"];
 
-			console.log(data);
-
-			/*
 			var dataH = {code:"CP00", children:[]};
 			var buildHierarchyFrom = function(root){
 				//find children codes in data
@@ -66,7 +63,7 @@
 			};
 			buildHierarchyFrom(dataH);
 			data = null;
-			 */
+
 			console.log(dataH);
 
 			var nodes = tree.nodes(dataH),
