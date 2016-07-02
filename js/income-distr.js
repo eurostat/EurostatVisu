14edc6dd0e7fd63f7a6f95ea3cccbe569f271807
julien Gaffuri <julien.gaffuri@gmail.com>
2016-07-02 18:24:11 +0200

    blank chart

--- js/income-distr.js
+++ js/income-distr.js
@@ -10,11 +10,11 @@
 
         //build svg element
         var margin = {top: 20, right: 20, bottom: 20, left: 20};
-        var width = 1000 - margin.left - margin.right, height = 600 - margin.top - margin.bottom;
+        var width = 500 - margin.left - margin.right, height = 300 - margin.top - margin.bottom;
         var chart = d3.select("#chart").append("svg")
                 .attr("width", width + margin.left + margin.right)
                 .attr("height", height + margin.top + margin.bottom)
-                .attr("transform", "translate("+margin.left+","+margin.top+ ")")
+                .append("g").attr("transform", "translate("+margin.left+","+margin.top+ ")")
             ;
 
         $.when(
@@ -37,15 +37,64 @@
 
                     //scales
                     var xScale = d3.scale.linear().domain([0,100]).range([0, width]);
-                    var yScale = d3.scale.linear().domain([0,100]).range([height, 0]);
+                    var yScale = d3.scale.linear().domain([0,100]).range([0, height]);
 
                     //clear previous
                     chart.selectAll("*").remove();
 
                     //draw chart
-                    chart.append("rect").attr("x",0).attr("y",0).attr("width",100).attr("height",100);
-                    //style="fill:blue;stroke:pink;stroke-width:5;fill-opacity:0.1;stroke-opacity:0.9" />
+                    //chart.append("rect").attr("fill","lightgray")
+                    //    .attr("y",yScale(0)).attr("x",xScale(0)).attr("width",xScale(100)).attr("height",yScale(100));
 
+                    //first 5 percentiles
+                    chart.append("rect").attr("class","disrect")
+                        .attr("y",yScale(0)).attr("x",xScale(0)).attr("width",100).attr("height",yScale(1));
+                    chart.append("rect").attr("class","disrect")
+                        .attr("y",yScale(1)).attr("x",xScale(0)).attr("width",50).attr("height",yScale(1));
+                    chart.append("rect").attr("class","disrect")
+                        .attr("y",yScale(2)).attr("x",xScale(0)).attr("width",100).attr("height",yScale(1));
+                    chart.append("rect").attr("class","disrect")
+                        .attr("y",yScale(3)).attr("x",xScale(0)).attr("width",50).attr("height",yScale(1));
+                    chart.append("rect").attr("class","disrect")
+                        .attr("y",yScale(4)).attr("x",xScale(0)).attr("width",100).attr("height",yScale(1));
+
+                    //second twentile
+                    chart.append("rect").attr("class","disrect")
+                        .attr("y",yScale(5)).attr("x",xScale(0)).attr("width",70).attr("height",yScale(5));
+
+                    //8 deciles in the middle
+                    chart.append("rect").attr("class","disrect")
+                        .attr("y",yScale(10)).attr("x",xScale(0)).attr("width",50).attr("height",yScale(10));
+                    chart.append("rect").attr("class","disrect")
+                        .attr("y",yScale(20)).attr("x",xScale(0)).attr("width",55).attr("height",yScale(10));
+                    chart.append("rect").attr("class","disrect")
+                        .attr("y",yScale(30)).attr("x",xScale(0)).attr("width",50).attr("height",yScale(10));
+                    chart.append("rect").attr("class","disrect")
+                        .attr("y",yScale(40)).attr("x",xScale(0)).attr("width",55).attr("height",yScale(10));
+                    chart.append("rect").attr("class","disrect")
+                        .attr("y",yScale(50)).attr("x",xScale(0)).attr("width",50).attr("height",yScale(10));
+                    chart.append("rect").attr("class","disrect")
+                        .attr("y",yScale(60)).attr("x",xScale(0)).attr("width",55).attr("height",yScale(10));
+                    chart.append("rect").attr("class","disrect")
+                        .attr("y",yScale(70)).attr("x",xScale(0)).attr("width",50).attr("height",yScale(10));
+                    chart.append("rect").attr("class","disrect")
+                        .attr("y",yScale(80)).attr("x",xScale(0)).attr("width",55).attr("height",yScale(10));
+
+                    //19th twentile
+                    chart.append("rect").attr("class","disrect")
+                        .attr("y",yScale(90)).attr("x",xScale(0)).attr("width",70).attr("height",yScale(5));
+
+                    //last 5 percentiles
+                    chart.append("rect").attr("class","disrect")
+                        .attr("y",yScale(95)).attr("x",xScale(0)).attr("width",100).attr("height",yScale(1));
+                    chart.append("rect").attr("class","disrect")
+                        .attr("y",yScale(96)).attr("x",xScale(0)).attr("width",50).attr("height",yScale(1));
+                    chart.append("rect").attr("class","disrect")
+                        .attr("y",yScale(97)).attr("x",xScale(0)).attr("width",100).attr("height",yScale(1));
+                    chart.append("rect").attr("class","disrect")
+                        .attr("y",yScale(98)).attr("x",xScale(0)).attr("width",50).attr("height",yScale(1));
+                    chart.append("rect").attr("class","disrect")
+                        .attr("y",yScale(99)).attr("x",xScale(0)).attr("width",100).attr("height",yScale(1));
                 };
                 updateChart();
 
