df376a4ca2ffb7781ec59c5b3e7d7b1479da2047
julien Gaffuri <julien.gaffuri@gmail.com>
2016-07-02 22:01:49 +0200

    simplification

--- js/income-distr.js
+++ js/income-distr.js
@@ -61,59 +61,27 @@
                     chart.append("rect").attr("fill","#eee")
                         .attr("y",yScale(0)).attr("x",xScale(0)).attr("width",xScale(100)).attr("height",yScale(100));
 
+
                     //draw chart rectangles
                     var rects = chart.append("g").attr("id","disrect");
 
+                    var addRect = function(group,value,pos,size){
+                        group.append("rect").attr("y",yScale(pos)).attr("x",xScale(0))
+                            .attr("width",xScale(value)).attr("height",yScale(size));
+                    };
+
+                    var i;
                     //first 5 percentiles
-                    rects.append("rect").attr("y",yScale(0)).attr("x",xScale(0))
-                        .attr("width",xScale(10*getValue("PERCENTILE1"))).attr("height",yScale(1));
-                    rects.append("rect").attr("y",yScale(1)).attr("x",xScale(0))
-                        .attr("width",xScale(10*getValue("PERCENTILE2"))).attr("height",yScale(1));
-                    rects.append("rect")
-                        .attr("y",yScale(2)).attr("x",xScale(0))
-                        .attr("width",xScale(10*getValue("PERCENTILE3"))).attr("height",yScale(1));
-                    rects.append("rect").attr("y",yScale(3)).attr("x",xScale(0))
-                        .attr("width",xScale(10*getValue("PERCENTILE4"))).attr("height",yScale(1));
-                    rects.append("rect").attr("y",yScale(4)).attr("x",xScale(0))
-                        .attr("width",xScale(10*getValue("PERCENTILE5"))).attr("height",yScale(1));
-
+                    for(i=0;i<=4;i++) addRect(rects,10*getValue("PERCENTILE"+(i+1)),i,1);
                     //second twentile
-                    rects.append("rect").attr("y",yScale(5)).attr("x",xScale(0))
-                        .attr("width",xScale(5*get2Twentilevalue())).attr("height",yScale(5));
-
+                    addRect(rects,5*get2Twentilevalue(),5,5);
                     //8 deciles in the middle
-                    rects.append("rect").attr("y",yScale(10)).attr("x",xScale(0))
-                        .attr("width",xScale(getValue("DECILE2"))).attr("height",yScale(10));
-                    rects.append("rect").attr("y",yScale(20)).attr("x",xScale(0))
-                        .attr("width",xScale(getValue("DECILE3"))).attr("height",yScale(10));
-                    rects.append("rect").attr("y",yScale(30)).attr("x",xScale(0))
-                        .attr("width",xScale(getValue("DECILE4"))).attr("height",yScale(10));
-                    rects.append("rect").attr("y",yScale(40)).attr("x",xScale(0))
-                        .attr("width",xScale(getValue("DECILE5"))).attr("height",yScale(10));
-                    rects.append("rect").attr("y",yScale(50)).attr("x",xScale(0))
-                        .attr("width",xScale(getValue("DECILE6"))).attr("height",yScale(10));
-                    rects.append("rect").attr("y",yScale(60)).attr("x",xScale(0))
-                        .attr("width",xScale(getValue("DECILE7"))).attr("height",yScale(10));
-                    rects.append("rect").attr("y",yScale(70)).attr("x",xScale(0))
-                        .attr("width",xScale(getValue("DECILE8"))).attr("height",yScale(10));
-                    rects.append("rect").attr("y",yScale(80)).attr("x",xScale(0))
-                        .attr("width",xScale(getValue("DECILE9"))).attr("height",yScale(10));
-
+                    for(i=2;i<=9;i++) addRect(rects,getValue("DECILE"+i),10*(i-1),10);
                     //19th twentile
-                    rects.append("rect").attr("y",yScale(90)).attr("x",xScale(0))
-                        .attr("width",xScale(5*get19Twentilevalue())).attr("height",yScale(5));
-
+                    addRect(rects,5*get19Twentilevalue(),90,5);
                     //last 5 percentiles
-                    rects.append("rect").attr("y",yScale(95)).attr("x",xScale(0))
-                        .attr("width",xScale(10*getValue("PERCENTILE95"))).attr("height",yScale(1));
-                    rects.append("rect").attr("y",yScale(96)).attr("x",xScale(0))
-                        .attr("width",xScale(10*getValue("PERCENTILE96"))).attr("height",yScale(1));
-                    rects.append("rect").attr("y",yScale(97)).attr("x",xScale(0))
-                        .attr("width",xScale(10*getValue("PERCENTILE97"))).attr("height",yScale(1));
-                    rects.append("rect").attr("y",yScale(98)).attr("x",xScale(0))
-                        .attr("width",xScale(10*getValue("PERCENTILE98"))).attr("height",yScale(1));
-                    rects.append("rect").attr("y",yScale(99)).attr("x",xScale(0))
-                        .attr("width",xScale(10*getValue("PERCENTILE99"))).attr("height",yScale(1));
+                    for(i=95;i<=99;i++) addRect(rects,10*getValue("PERCENTILE"+i),i,1);
+
                 };
                 updateChart();
 
