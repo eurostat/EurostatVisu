857d15f35297c90e46fd53a7c5432e04e7012e1e
julien Gaffuri <julien.gaffuri@gmail.com>
2016-07-03 18:06:59 +0200

    structure change

--- js/income-distr.js
+++ js/income-distr.js
@@ -17,10 +17,16 @@
                 .append("g").attr("transform", "translate("+margin.left+","+margin.top+ ")")
             ;
 
+        //build legend svg element
+        var lgd = d3.select("#legend").append("svg").attr("width", width).attr("height", height + margin.top + margin.bottom);
+        var topLgd = 10;
+
+
         $.when(
             //get income distribution data
             $.ajax({url:EstLib.getEstatDataURL("ilc_di01",{currency:"EUR",indic_il:"SHARE",quantile:quantiles})})
         ).then(function(data) {
+                var i;
 
                 //simplify geo label texts
                 EstLib.overrideCountryNames(data.dimension.geo.category.label);
@@ -28,88 +34,13 @@
                 //decode data
                 data = JSONstat(data).Dataset(0);
 
-                var updateChart = function(){
 
-                    var getValue = function(quantile){
-                        var d = data.Data({currency:"EUR",indic_il:"SHARE",time:timeSel,geo:geoSel,quantile:quantile});
-                        if(!d) return 0;
-                        return d.value || 0;
-                    };
-
-                    //TODO correct
-                    var get2Twentilevalue = function(){
-                        var v = getValue("DECILE1");
-                        for(i=1;i<=5;i++) v -= getValue("PERCENTILE"+i);
-                        return v;
-                    };
-                    //TODO correct
-                    var get19Twentilevalue = function(){
-                        var v = getValue("DECILE10");
-                        for(i=95;i<=99;i++) v -= getValue("PERCENTILE"+i);
-                        return v;
-                    };
-
-                    //scales
-                    var xScale = d3.scale.linear().domain([0,50]).range([0, width]); //TODO adapt max
-                    var yScale = d3.scale.linear().domain([0,100]).range([0, height]);
-
-                    //clear previous
-                    chart.selectAll("*").remove();
-
-                    //background
-                    //chart.append("rect").attr("fill","#eee").attr("y",yScale(0)).attr("x",xScale(0)).attr("width",xScale(100)).attr("height",yScale(100));
-
-                    //draw chart rectangles
-                    var rects = chart.append("g").attr("id","disrect");
-
-                    var addRect = function(group,value,pos,size){
-                        group.append("rect").attr("y",yScale(pos)).attr("x",xScale(0))
-                            .attr("width",xScale(value)).attr("height",yScale(size));
-                    };
-
-                    var i;
-                    //first 5 percentiles
-                    for(i=0;i<=4;i++) addRect(rects,10*getValue("PERCENTILE"+(i+1)),i,1);
-                    //second twentile
-                    addRect(rects,2*get2Twentilevalue(),5,5);
-                    //8 deciles in the middle
-                    for(i=2;i<=9;i++) addRect(rects,1*getValue("DECILE"+i),10*(i-1),10);
-                    //19th twentile
-                    addRect(rects,2*get19Twentilevalue(),90,5);
-                    //last 5 percentiles
-                    for(i=95;i<=99;i++) addRect(rects,10*getValue("PERCENTILE"+i),i,1);
-
-                };
-
-                //TODO
+                //the selection
+                //TODO get from URL
                 var geoSel = "EU28";
                 var timeSel = "2013";
 
-                var showGeo = function(geo){
-                    geoSel=geo; updateChart();
-                    //bold legend label
-                    d3.selectAll(".lgdG").attr("font-weight","normal").attr("fill","black");
-                    d3.selectAll(".lgdG[geo="+geo+"]").attr("font-weight","bold").attr("fill","maroon");
-                    //title
-                    d3.select("#geoTitle").text(geo);
-                };
-                //var hideGeo = function(geo){};
-                var showTime = function(time){
-                    timeSel=time; updateChart();
-                    //bold legend label
-                    d3.selectAll(".lgdT").attr("font-weight","normal").attr("fill","black");
-                    d3.selectAll(".lgdT[time='"+time+"']").attr("font-weight","bold").attr("fill","maroon");
-                    //title
-                    d3.select("#timeTitle").text(time);
-                };
-                //var hideTime = function(time){};
-
-                //legend
-                //width = 100;
-                var lgd = d3.select("#legend").append("svg").attr("width", width).attr("height", height + margin.top + margin.bottom);
-                var topLgd = 10;
-
-                //geo legend TODO better show
+                //fill geo legend TODO better show
                 var geos = data.Dimension("geo").id;
                 geos.sort(EstLib.geoComparison);
                 var dy = margin.top+topLgd;
@@ -119,11 +50,11 @@
                     .attr("x", 0)
                     .attr("y", function() { dy+=10; return dy-10; })
                     .text(function(d) { return d; })
-                    .on("mouseover", function() { showGeo(d3.select(this).attr("geo")); })
+                    .on("mouseover", function() { geoSel=d3.select(this).attr("geo"); update(); })
                     //.on("mouseout", function() { hideGeo(d3.select(this).attr("geo")); })
                 ;
 
-                //time legend
+                //fill time legend
                 var times = data.Dimension("time").id;
                 dy = margin.top+topLgd;
                 lgd.append("g").selectAll("text").data(times).enter().append("text")
@@ -132,11 +63,95 @@
                     .attr("x", 60)
                     .attr("y", function() { dy+=10; return dy-10; })
                     .text(function(d) { return d; })
-                    .on("mouseover", function() { showTime(d3.select(this).attr("time")); })
+                    .on("mouseover", function() { timeSel= d3.select(this).attr("time"); update(); })
                     //.on("mouseout", function() { hideTime(d3.select(this).attr("time")); })
                 ;
 
-                updateChart();
+
+                //try to retrieve a quantile value
+                var getValue = function(quantile){
+                    var d = data.Data({currency:"EUR",indic_il:"SHARE",time:timeSel,geo:geoSel,quantile:quantile});
+                    if(!d) return 0;
+                    return d.value || 0;
+                };
+
+                //compute second twentile from first decile and five first percentiles
+                var get2Twentilevalue = function(){
+                    //TODO correct
+                    var v = getValue("DECILE1");
+                    for(i=1;i<=5;i++) v -= getValue("PERCENTILE"+i);
+                    return v;
+                };
+                //compute 19th twentile from last decile and five last percentiles
+                var get19Twentilevalue = function(){
+                    //TODO correct
+                    var v = getValue("DECILE10");
+                    for(i=95;i<=99;i++) v -= getValue("PERCENTILE"+i);
+                    return v;
+                };
+
+                //check if percentile data is available
+                var detailledDataPresent = function(first){
+                    //TODO
+                    //check presence of 5 percentiles
+                    //var d = data.Data({currency:"EUR",indic_il:"SHARE",time:timeSel,geo:geoSel,quantile:quantile});
+                    return true;
+                };
+
+
+                //update the chart
+                var update = function(){
+
+                    //scales
+                    var xScale = d3.scale.linear().domain([0,50]).range([0, width]); //TODO adapt max
+                    var yScale = d3.scale.linear().domain([0,100]).range([0, height]);
+
+                    //clear previous
+                    chart.selectAll("*").remove();
+
+                    //distribution rectangles group
+                    var rects = chart.append("g").attr("id","disrect");
+
+                    //draw distribution rectangle
+                    var addRect = function(value,pos,size){
+                        rects.append("rect").attr("y",yScale(pos)).attr("x",xScale(0))
+                            .attr("width",xScale(value)).attr("height",yScale(size));
+                        //TODO onmouseover
+                    };
+
+                    if(detailledDataPresent(true)){
+                        //first 5 percentiles
+                        for(i=0;i<=4;i++) addRect(10*getValue("PERCENTILE"+(i+1)),i,1);
+                        //second twentile
+                        addRect(2*get2Twentilevalue(),5,5);
+                    } else {
+                        //first decile
+                        addRect(1*getValue("DECILE1"),0,10);
+                    }
+                    //8 deciles in the middle
+                    for(i=2;i<=9;i++) addRect(1*getValue("DECILE"+i),10*(i-1),10);
+                    if(detailledDataPresent(false)){
+                        //19th twentile
+                        addRect(2*get19Twentilevalue(),90,5);
+                        //last 5 percentiles
+                        for(i=95;i<=99;i++) addRect(10*getValue("PERCENTILE"+i),i,1);
+                    } else {
+                        //last decile
+                        addRect(1*getValue("DECILE10"),90,10);
+                    }
+
+                    //bold geo legend label
+                    d3.selectAll(".lgdG").attr("font-weight","normal").attr("fill","black");
+                    d3.selectAll(".lgdG[geo="+geoSel+"]").attr("font-weight","bold").attr("fill","maroon");
+                    //bold time legend label
+                    d3.selectAll(".lgdT").attr("font-weight","normal").attr("fill","black");
+                    d3.selectAll(".lgdT[time='"+timeSel+"']").attr("font-weight","bold").attr("fill","maroon");
+                    //update title
+                    d3.select("#geoTitle").text(geoSel); //TODO text!
+                    d3.select("#timeTitle").text(timeSel);
+                };
+
+                update();
 
             }, function() {
                 console.log("Could not load data");
