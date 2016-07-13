93134ff5cc08d63058f938225e9d373986734db7
julien Gaffuri <julien.gaffuri@gmail.com>
2016-07-13 20:23:21 +0200

    join

--- js/join.js
+++ js/join.js
@@ -8,21 +8,21 @@
         //https://bost.ocks.org/mike/join/
 
         var data = [
-            {x:10,y:10},
-        ]; //TODO
+            {x:10,y:10}
+        ];
 
         var circle =
             d3.select("#join").append("svg").attr("width", 100).attr("height", 100)
                 .selectAll("circle")
                 .data(data);
 
-        circle.exit().remove();
-
-        circle.enter().append("circle").attr("r", 2.5);
-
-        circle
+        circle.enter().append("circle")
             .attr("cx", function(d) { return d.x; })
-            .attr("cy", function(d) { return d.y; });
+            .attr("cy", function(d) { return d.y; })
+            .attr("r", 0).transition().attr("r", 2.5);
+        circle.exit().transition().attr("r", 0).remove();
+
+
 
     });
 }(jQuery));
