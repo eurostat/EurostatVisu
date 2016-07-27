572f447c0434f20bf9f2c0f481a9518a72667bd8
jgaffuri <julien.gaffuri@ec.europa.eu>
2016-07-27 17:08:11 +0200

    a

---
+++ js/income_distr.js
@@ -0,0 +1,76 @@
+/**
+ *
+ * Generic functions for income distribution visualisations
+ *
+ * @author julien Gaffuri
+ *
+ */
+(function($, EstLib) {
+
+	EstLib.dictIncomeDistr = {
+            en:{
+                title:"Income disparities in Europe",
+                title1:"Income disparities",
+                title2:"in",
+                countryText:"Country",
+                yearText:"Year",
+                avincome:"Average income",
+                lowincome:"Lowest incomes",
+                higincome:"Highest incomes",
+                incomelev:"Income level",
+                incomeof:"The income of the",
+                ofpopwith:"of the population with the",
+                lowest:"lowest",
+                highest:"highest",
+                incomeis:"income is",
+                oftotal:"of the total country income",
+                ifincomewas:"If the country income was equally distributed, it would be",
+                thisincis:"This income is",
+                times:"times",
+                higher:"higher",
+                lower:"lower",
+                thanav:"than the average income",
+                percent:"percent",
+                twentieth:"twentieth",
+                twentyfifth:"twentyfifth",
+                tenth:"tenth",
+                sourceTxt:"Sources: <a href='http://ec.europa.eu/eurostat/'>Eurostat</a> databases on income distribution (<a href='http://ec.europa.eu/eurostat/web/income-and-living-conditions/overview'>EU-SILC</a>). More information <a href='http://ec.europa.eu/eurostat/statistics-explained/index.php/Income_distribution_statistics'>here</a>.",
+                focusCntr:"Focus on country comparison.",
+                nodata:"No data"
+            },
+            fr:{
+                title:"Disparités de revenus en Europe",
+                title1:"Disparités de revenus",
+                title2:"en",
+                countryText:"Pays",
+                yearText:"Année",
+                avincome:"Revenu moyen",
+                lowincome:"Bas revenus",
+                higincome:"Hauts revenus",
+                incomelev:"Niveau de revenu",
+                incomeof:"Les revenus du",
+                ofpopwith:"de la population avec les",
+                lowest:"plus bas",
+                highest:"plus hauts",
+                incomeis:"revenus est",
+                oftotal:"du total des revenus du pays",
+                ifincomewas:"Si les revenus étaient équitablement répartis, ce serait",
+                thisincis:"Ces revenus sont",
+                times:"fois",
+                higher:"plus hauts",
+                lower:"plus bas",
+                thanav:"que le revenu moyen",
+                percent:"pourcent",
+                twentieth:"vingtième",
+                twentyfifth:"vingtcinquième",
+                tenth:"dixième",
+                sourceTxt:"Sources: Bases de données <a href='http://ec.europa.eu/eurostat/'>Eurostat</a> sur la répartition des revenus (<a href='http://ec.europa.eu/eurostat/web/income-and-living-conditions/overview'>EU-SILC</a>). Plus d'information <a href='http://ec.europa.eu/eurostat/statistics-explained/index.php/Income_distribution_statistics'>ici</a>.",
+                focusCntr:"Comparaison par pays.",
+                nodata:"Pas de données"
+            }
+        };
+
+	
+	
+
+}(jQuery, window.EstLib = window.EstLib || {} ));
