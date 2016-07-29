cd715176806b66204fbae802eb1d79a84d35e77e
jgaffuri <julien.gaffuri@ec.europa.eu>
2016-07-29 10:04:14 +0200

    bug fix in tooltip

--- js/income_distr.js
+++ js/income_distr.js
@@ -132,10 +132,9 @@
 
 
     //build the text to explain a chart rectangle corresponding to a quantile
-    EstLib.getRectText = function(d, value, lg){
-        var html = [], quantileNb = +d.substring(1,d.length), q = EstLib.quantileDict[d.charAt(0)],
+    EstLib.getRectText = function(qu, value, dict, lg){
+        var html = [], quantileNb = +qu.substring(1,qu.length), q = EstLib.quantileDict[qu.charAt(0)],
             lowestIncome = 100/(quantileNb*q.percentage) >= 2, coeff = value/q.percentage
-        dict = EstLib.dictIncomeDistr[lg] || EstLib.dictIncomeDistr.en;
         html.push(
             dict.incomeof, " <b>",
             EstLib.getNumbered(lowestIncome?quantileNb:100/q.percentage-quantileNb+1, lg),
