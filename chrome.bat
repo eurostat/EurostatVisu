1f0bebe3c2aed485434db70aeebfd25b69be7211
jgaffuri <julien.gaffuri@ec.europa.eu>
2016-08-16 15:30:22 +0200

    a

--- chrome.bat
+++ chrome.bat
@@ -1,2 +1,2 @@
 REM "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" --allow-file-access-from-files H:\workspace\EurostatVisu\coicop_hierarchy.html
-"C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" --allow-file-access-from-files H:\workspace\EurostatVisu\nuts_map_ex.html
+"C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" --allow-file-access-from-files H:\workspace\EurostatVisu\population_map.html
