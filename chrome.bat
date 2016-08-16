4b856520131df3c7e96d799460b7b103a36f5485
jgaffuri <julien.gaffuri@ec.europa.eu>
2016-08-16 17:25:30 +0200

    cleaning

--- chrome.bat
+++
@@ -1,2 +0,0 @@
-REM "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" --allow-file-access-from-files H:\workspace\EurostatVisu\coicop_hierarchy.html
-"C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" --allow-file-access-from-files H:\workspace\EurostatVisu\population_map.html
