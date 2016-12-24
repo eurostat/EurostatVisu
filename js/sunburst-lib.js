62a1df042a8274f840c72adb7c8aae01b7f0b040
julien Gaffuri <julien.gaffuri@gmail.com>
2016-12-24 23:33:37 +0100

    sunburst generic lib

---
+++ js/sunburst-lib.js
@@ -0,0 +1,22 @@
+/**
+ *
+ * Sunburst visualisation
+ *
+ * @author julien Gaffuri
+ *
+ */
+(function(d3) {
+
+    d3.sunburst = function(div){
+        div = div || "sunburst";
+
+        var out = {};
+
+        out.set = function(data){
+
+        }
+
+        return out;
+    }
+
+}(d3));
